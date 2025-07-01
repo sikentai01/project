a2a9350ca7237d6e1d7f1e1301f013f04db517fa
aki nami <akito0687@icloud.com>
2025-07-02 00:21:26 +0900

    feat: Add Tiled map data and SuperTiled2Unity

---
+++ Assets/Map/boeRTaV-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="boeRTaV-1" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="コミュ将の素材倉庫/boeRTaV-1.png" width="768" height="768"/>
+</tileset>
