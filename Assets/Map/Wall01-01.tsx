116a77b7391c37d1e8657e97115a89310a4278a2
aki nami <akito0687@icloud.com>
2025-07-11 23:32:50 +0900

    心臓,廊下1追加

--- Assets/Map/Wall01-01.tsx
+++ Assets/Map/Wall01-01.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Wall01-01" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="ドット絵世界/Wall01-01.png" width="96" height="96"/>
+<tileset version="1.10" tiledversion="1.11.2" name="Wall01-01" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="ドット絵世界/Wall01-01.png" width="96" height="128"/>
 </tileset>
