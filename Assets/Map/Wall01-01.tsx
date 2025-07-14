6c92fdb1a09647ac9da451fdcaec1e0bdddcf509
aki nami <akito0687@icloud.com>
2025-07-15 02:18:05 +0900

    コライダー変更

--- Assets/Map/Wall01-01.tsx
+++ Assets/Map/Wall01-01.tsx
@@ -46,9 +46,4 @@
    <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
- <tile id="9">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
 </tileset>
