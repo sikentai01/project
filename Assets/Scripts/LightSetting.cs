a19c79cfca9797ea4bc8ef529874e44db1394bbc
sikentai01 <138580371+sikentai01@users.noreply.github.com>
2025-07-14 18:31:13 +0900

    ui

---
+++ Assets/Scripts/LightSetting.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+public class LigitSetting : MonoBehaviour{
+    public Light2D lightSource; // ライトの参照
+    public float intensity = 1f; // ライトの強度
+
+    void Start()
+    {
+        if (lightSource == null)
+        {
+            lightSource = GetComponent <Light2D>();
+        }
+        UpdateLightSettings();
+    }
+
+    void UpdateLightSettings()
+    {
+        if (lightSource != null)
+        {
+            lightSource.intensity = intensity;
+        }
+    }
+
+    void OnValidate()
+    {
+        UpdateLightSettings();
+    }
+}
