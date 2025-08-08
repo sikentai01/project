e3a89ecc94c929aeccc08d7576dbeb254869cdd5
aki nami <akito0687@icloud.com>
2025-08-08 18:10:04 +0900

    作業継続中、緊急フォルダ整理

--- Assets/Map/boeRTaV48-1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="boeRTaV48-1" tilewidth="32" tileheight="32" tilecount="12" columns="6">
- <image source="コミュ将の素材倉庫/boeRTaV48-1.png" width="192" height="64"/>
-</tileset>
--- Assets/Map/mU8QYpo-1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="mU8QYpo-1" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="コミュ将の素材倉庫/mU8QYpo-1.png" width="128" height="128"/>
-</tileset>
