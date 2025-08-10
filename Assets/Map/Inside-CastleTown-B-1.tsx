2652f0fd1b6f81362c5b2b3866fd1c82700b57c8
aki nami <akito0687@icloud.com>
2025-08-10 18:15:56 +0900

    一時提出

---
+++ Assets/Map/Inside-CastleTown-B-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Inside-CastleTown-B-1" tilewidth="32" tileheight="32" tilecount="3" columns="1">
+ <image source="ドット絵世界/Inside-CastleTown-B-1.png" width="32" height="96"/>
+</tileset>
