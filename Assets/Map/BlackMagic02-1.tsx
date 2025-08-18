b8d49f578fe5f6a28d4b4625d9e23903df66ed8c
aki nami <akito0687@icloud.com>
2025-08-18 17:09:39 +0900

    モーション、マップ等

---
+++ Assets/Map/BlackMagic02-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="BlackMagic02-1" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="ドット絵世界/BlackMagic02-1.png" width="96" height="128"/>
+</tileset>
---
+++ Assets/Map/肝臓ギミック1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="肝臓ギミック1 " tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="個人作/肝臓ギミック1 .png" width="96" height="128"/>
+</tileset>
