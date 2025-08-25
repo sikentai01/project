0c717cb900f94d459ef73a62cc07348fcac28e0d
aki nami <akito0687@icloud.com>
2025-08-25 18:18:54 +0900

    モーションかんせー！やったー！

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,9 @@ public class GridMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float gridSize = 1f;
+
     private Vector3 targetPosition;
     private bool isMoving = false;
-    private bool Move_motion = true;
     private Animator animator;
 
     void Start()
@@ -17,35 +17,51 @@ public class GridMovement : MonoBehaviour
 
     void Update()
     {
-        if (!isMoving)
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        int key = -1;
+
+        // アニメーションを制御するロジックを分離
+        if (horizontalInput != 0 || verticalInput != 0)
         {
-            float horizontalInput = Input.GetAxisRaw("Horizontal");
-            float verticalInput = Input.GetAxisRaw("Vertical");
-
-            Vector3 nextPos = Vector3.zero;
-
-            if (horizontalInput != 0)
+            animator.SetBool("Move_motion", true);
+            if (horizontalInput > 0&&key!=2)
             {
-                nextPos = transform.position + new Vector3(horizontalInput * gridSize, 0, 0);
-                animator.SetBool("Move_motion", true);
-                if (horizontalInput > 0) animator.SetInteger("Direction", 2); // 右
-                else if (horizontalInput < 0) animator.SetInteger("Direction", 1); // 左
+                animator.SetInteger("Direction", 2);
+                key = 2;
             }
-            else if (verticalInput != 0)
+            else if (horizontalInput < 0&&key!=1)
             {
-                nextPos = transform.position + new Vector3(0, verticalInput * gridSize, 0);
-                animator.SetBool("Move_motion", true);
-                if (verticalInput > 0) animator.SetInteger("Direction", 3); // 上
-                else if (verticalInput < 0) animator.SetInteger("Direction", 0); // 下
+                animator.SetInteger("Direction", 1);
+                key = 1;
             }
+            else if (verticalInput > 0 && key != 3)
+            {
+                animator.SetInteger("Direction", 3);
+                key = 3;
+            }
+            else if (verticalInput < 0 && key != 0)
+            {
+                animator.SetInteger("Direction", 0);
+                key = 0;
+            }
+        }
+        else
+        {
+            animator.SetBool("Move_motion", false);
+            key = -1;
+        }
 
-            // 次の場所に移動可能かチェック
-            if (nextPos != Vector3.zero && !IsOccupied(nextPos))
+        // 移動ロジック
+        if (!isMoving && (horizontalInput != 0 || verticalInput != 0))
+        {
+            Vector3 nextPos = transform.position + new Vector3(horizontalInput * gridSize, verticalInput * gridSize, 0);
+
+            if (!IsOccupied(nextPos))
             {
                 targetPosition = nextPos;
                 isMoving = true;
             }
-
         }
 
         if (isMoving)
@@ -61,10 +77,7 @@ public class GridMovement : MonoBehaviour
 
     bool IsOccupied(Vector3 nextPos)
     {
-        // nextPosの中心から少しY軸を下にずらす
-        Vector3 checkPos = nextPos + new Vector3(0, -0.5f, 0); // キャラクターの足元をチェック
-
-        // Physics2D.OverlapBoxを使用し、コライダーのサイズを調整
+        Vector3 checkPos = nextPos + new Vector3(0, -0.5f, 0);
         Collider2D hitCollider = Physics2D.OverlapBox(checkPos, new Vector2(0.8f, 0.8f), 0f);
 
         if (hitCollider != null && hitCollider.gameObject != gameObject)
