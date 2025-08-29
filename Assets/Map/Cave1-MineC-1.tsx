afc7375b7511ac982794003e910f1f3beaa74fdb
aki nami <akito0687@icloud.com>
2025-08-29 13:49:39 +0900

    map追加、小腸、階段、廊下

---
+++ Assets/Map/Cave1-MineC-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Cave1-MineC-1" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="ドット絵世界/Cave1-MineC-1.png" width="128" height="64"/>
+</tileset>
---
+++ Assets/Map/Cave1-MineC.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Cave1-MineC" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="ドット絵世界/Cave1-MineC.png" width="512" height="512"/>
+</tileset>
