34b2e460d0006a1590a4b1eef30e722ff7ff0e3b
aki nami <akito0687@icloud.com>
2025-09-03 18:09:39 +0900

    c

--- Assets/Map/!$Gate-Euro03.tsx
+++ Assets/Map/!$Gate-Euro03.tsx
@@ -1,34 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="!$Gate-Euro03" tilewidth="32" tileheight="32" tilecount="56" columns="7">
  <image source="ドット絵世界/!$Gate-Euro03.png" width="240" height="256"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
- <tile id="1">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
- <tile id="2">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
- <tile id="7">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
- <tile id="8">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
- <tile id="9">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="32" height="32"/>
-  </objectgroup>
- </tile>
 </tileset>
