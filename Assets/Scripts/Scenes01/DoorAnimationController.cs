0d72a52ad0d5b9da8ddf02208489c7aeb3b46c3c
Imai-Koudai <imakoda@icloud.com>
2025-09-03 19:27:22 +0900

    a

---
+++ Assets/Scripts/Scenes01/DoorAnimationController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class DoorAnimationController : MonoBehaviour
+{
+    // インスペクターからキャラクターのAnimatorを割り当てる
+    public Animator characterAnimator;
+
+    // プレイヤーがコライダー内にいるかどうかのフラグ
+    private bool playerIsNearDoor = false;
+
+    // トリガーコライダーに入ったときに呼び出される
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // プレイヤーに"Player"タグがついているか確認
+        if (other.CompareTag("Player"))
+        {
+            playerIsNearDoor = true;
+        }
+    }
+
+    // トリガーコライダーから出たときに呼び出される
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // プレイヤーに"Player"タグがついているか確認
+        if (other.CompareTag("Player"))
+        {
+            playerIsNearDoor = false;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // プレイヤーがドアの近くにいて、エンターキーが押されたかチェック
+        if (playerIsNearDoor && Input.GetKeyDown(KeyCode.Return))
+        {
+            // Animatorにトリガーをセットして、アニメーションを再生
+            characterAnimator.SetTrigger("OpenGateTrigger");
+        }
+    }
+}
