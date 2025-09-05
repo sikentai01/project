f9ce6c2492baba7c7bfad3e93806911dc710de5a
Imai-Koudai <imakoda@icloud.com>
2025-09-06 01:41:33 +0900

    アニメーション

--- Assets/Scripts/Scenes01/DoorAnimationController.cs
+++ Assets/Scripts/Scenes01/DoorAnimationController.cs
@@ -1,48 +1,57 @@
 using UnityEngine;
+using System.Collections;
 
 public class DoorAnimationController : MonoBehaviour
 {
-    // インスペクターからキャラクターのAnimatorを割り当てる
     public Animator characterAnimator;
+    public MonoBehaviour movementScript;
 
-    // プレイヤーがコライダー内にいるかどうかのフラグ
-    private bool playerIsNearDoor = false;
+    private bool isAnimationPlaying = false;
 
-    // トリガーコライダーに入ったときに呼び出される
+    // トリガーコライダーに入ったときに一度だけ呼び出される
     void OnTriggerEnter2D(Collider2D other)
     {
-        // プレイヤーに"Player"タグがついているか確認
-        if (other.CompareTag("Player"))
+        // Debug.Log("ドアに近づきました"); // デバッグ用
+    }
+
+    // トリガーコライダーの中にいる間、毎フレーム呼び出される
+    void OnTriggerStay2D(Collider2D other)
+    {
+        // アニメーション再生中でない、かつEnterキーが押された場合
+        if (!isAnimationPlaying && other.CompareTag("Player") && Input.GetKeyDown(KeyCode.Return))
         {
-            playerIsNearDoor = true;
-            Debug.Log("プレイヤーがドアの近くに入りました"); // ★これを追加
+            isAnimationPlaying = true;
+
+            // 移動スクリプトを一時的に無効化する
+            if (movementScript != null)
+            {
+                movementScript.enabled = false;
+            }
+
+            // アニメーションを再生
+            characterAnimator.Play("OpenGate");
+
+            // アニメーションが終了するまで待機するコルーチンを開始
+            StartCoroutine(WaitForAnimationEnd());
         }
     }
 
     // トリガーコライダーから出たときに呼び出される
     void OnTriggerExit2D(Collider2D other)
     {
-        // プレイヤーに"Player"タグがついているか確認
-        if (other.CompareTag("Player"))
-        {
-            playerIsNearDoor = false;
-            Debug.Log("プレイヤーがドアから離れました"); // ★これを追加
-        }
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+        // Debug.Log("ドアから離れました"); // デバッグ用
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator WaitForAnimationEnd()
     {
-        // プレイヤーがドアの近くにいて、エンターキーが押されたかチェック
-        if (playerIsNearDoor && Input.GetKeyDown(KeyCode.Return))
+        yield return null;
+
+        yield return new WaitForSeconds(characterAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+        if (movementScript != null)
         {
-            // Animatorにトリガーをセットして、アニメーションを再生
-            characterAnimator.SetTrigger("OpenGateTrigger");
+            movementScript.enabled = true;
         }
+        isAnimationPlaying = false;
     }
 }
