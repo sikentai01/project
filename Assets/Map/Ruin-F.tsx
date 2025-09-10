84ff324e6e82747483bc908806f74a9d253aeab2
aki nami <akito0687@icloud.com>
2025-09-10 18:50:01 +0900

    map

--- Assets/Map/Ruin-F.tsx
+++ Assets/Map/Ruin-F.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Ruin-F" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="ドット絵世界/Ruin-F.png" width="512" height="512"/>
+ <tile id="96">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="112">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
