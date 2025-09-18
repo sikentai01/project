933232be70d3c9b0bf30ac81777edfb06ac19c0e
aki nami <akito0687@icloud.com>
2025-09-18 15:55:18 +0900

    map

---
+++ Assets/Map/ST-Toilet-I01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ST-Toilet-I01" tilewidth="32" tileheight="32" tilecount="344" columns="8">
+ <image source="ドット絵世界/ST-Toilet-I01.png" width="256" height="1376"/>
+</tileset>
