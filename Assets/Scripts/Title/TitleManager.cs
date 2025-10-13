fb1c1bad36fac54919e884df9abf4d0859df9ce1
Shirayuki-1722 <rainbowo926@icloud.com>
2025-10-13 22:29:29 +0900

    a

--- Assets/Scripts/Title/TitleManager.cs
+++ Assets/Scripts/Title/TitleManager.cs
@@ -1,14 +1,48 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class TitleMenuManager : MonoBehaviour
 {
-    [SerializeField] private GameObject firstSelectedButton; // ← Start ボタンをアサイン
+    [Header("初期選択ボタン設定")]
+    [SerializeField] private GameObject firstSelectedButton; // Startボタンをアサイン
 
-    void Start()
+    private void Start()
     {
-        // シーン開始時に START ボタンを選択状態にする
+        // タイトル開始時に START ボタンを選択状態にする
         EventSystem.current.SetSelectedGameObject(null);
         EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+    }
+
+    // --- 各ボタン用処理 ---
+
+    /// <summary>
+    /// 「初めから」ボタンを押したときに Scene0 に移動
+    /// </summary>
+    public void OnStartGame()
+    {
+        // シーン名かビルドインデックスどちらでもOK（ここでは名前で指定）
+        SceneManager.LoadScene("Scenes0");
+    }
+
+    /// <summary>
+    /// 「つづきから」ボタン用（未実装でもOK）
+    /// </summary>
+    public void OnLoadGame()
+    {
+        Debug.Log("ロード機能は後で追加予定");
+    }
+
+    /// <summary>
+    /// 「終了」ボタンを押したとき
+    /// </summary>
+    public void OnExitGame()
+    {
+        Debug.Log("ゲーム終了");
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // エディタ中なら再生停止
+#endif
     }
 }
