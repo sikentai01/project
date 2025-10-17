6332bf366fb955b750d9127492b393aa0918182b
Shirayuki-1722 <rainbowo926@icloud.com>
2025-10-17 19:57:48 +0900

    a

--- Assets/Map/ModernI-A501.tsx
+++ Assets/Map/ModernI-A501.tsx
@@ -1,6 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="ModernI-A501" tilewidth="32" tileheight="32" tilecount="128" columns="8">
  <image source="ドット絵世界/ModernI-A501.png" width="256" height="512"/>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="50">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="32"/>
