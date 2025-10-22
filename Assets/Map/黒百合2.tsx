3e2b4a78fa74bc0b740a504397981f6c0ba40c66
aki nami <akito0687@icloud.com>
2025-10-22 23:15:18 +0900

    黒百合

---
+++ Assets/Map/黒百合2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="黒百合2" tilewidth="32" tileheight="32" tilecount="8" columns="2">
+ <image source="ドット絵世界/黒百合2.png" width="64" height="128"/>
+</tileset>
