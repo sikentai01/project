1ee60e89cbb9d02067adebf35374142b03ac7c03
aki nami <akito0687@icloud.com>
2025-10-22 17:41:11 +0900

    肝臓これでいけるはず

---
+++ Assets/Map/肝臓ギミック3影.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="肝臓ギミック3影" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="個人作/肝臓ギミック3影.png" width="32" height="64"/>
+</tileset>
